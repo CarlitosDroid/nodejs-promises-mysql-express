8b2d9d1700de77901cc4bda96f2be24218aa80b2
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-03-02 23:01:38 -0500

    chore(init):init

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Carlos Leonardo Camilo Vargas Huamán on 3/2/17.
+ */
+
+var config = require("./config/config");
+
+console.log(config.port);
+console.log(config.db);
+console.log(config.db.host);
+
+
---
+++ config/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Carlos Leonardo Camilo Vargas Huamán on 3/2/17.
+ */
+
+module.exports = {
+    port : 8123,
+    db : {
+        host: "localhost"
+    }
+};
