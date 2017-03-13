63e5109e7c8ae664c3686d25c347e46197247e12
Carlos Leonardo Camilo Vargas Huamán <carlosleonardovargashuaman@gmail.com>
2017-03-12 22:43:32 -0500

    script modified

--- our-event-service/api/config/config.js
+++ our-event-service/api/config/config.js
@@ -2,6 +2,7 @@
  * Created by Carlos Leonardo Camilo Vargas Huamán on 3/2/17.
  */
 
+/*
 module.exports = {
     port : 8123,
     db : {
@@ -9,6 +10,17 @@ module.exports = {
         database: "CHIRINOS",
         user: "root",
         password: "",
+        port: 3306
+    }
+};*/
+
+module.exports = {
+    port: process.env.PORT || 8123,
+    db: {
+        host: process.env.DATABASE_HOST || '127.0.0.1',
+        database: 'CHIRINOS',
+        user: 'users_service',
+        password: '123',
         port: 3306
     }
 };
